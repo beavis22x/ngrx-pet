1742c730817c50029f4b77a0cfd1b740417dddf5
beavis22x <karakatuya@gmail.com>
2022-12-20 17:10:53 +0300

    feat(auth): add auth mmodules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import { getCurrentUserAction } from 'src/app/auth/store/actions/getCurrentUser.action';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+  selector: ' app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ngrx-1';
+export class AppComponent implements OnInit {
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(getCurrentUserAction());
+  }
 }
