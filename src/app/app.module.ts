14217760491ea29aa9648dab5950b8461d223221
beavis22x <karakatuya@gmail.com>
2022-12-21 12:24:34 +0300

    feat(article): add create/edit article modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { StoreModule } from '@ngrx/store'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
+import { routerReducer, StoreRouterConnectingModule } from '@ngrx/router-store'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AuthModule } from 'src/app/auth/auth.module';
-import { environment } from 'src/environments/environment';
-import { EffectsModule } from '@ngrx/effects';
-import { TopBarModule } from 'src/app/shared/modules/topBar/topBar.module';
-import { PersistanceService } from './shared/services/persistance.service';
-import { AuthInterceptor } from './shared/services/authinterceptor.service';
-import { GlobalFeedModule } from './globalFeed/globalFeed.module';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { AuthModule } from 'src/app/auth/auth.module'
+import { environment } from 'src/environments/environment'
+import { EffectsModule } from '@ngrx/effects'
+import { TopBarModule } from 'src/app/shared/modules/topBar/topBar.module'
+import { PersistanceService } from './shared/services/persistance.service'
+import { AuthInterceptor } from './shared/services/authinterceptor.service'
+import { GlobalFeedModule } from './globalFeed/globalFeed.module'
+import { YourFeedModule } from './yourFeed/yourFeed.module'
+import { TagFeedModule } from 'src/app/tagFeed/tagFeed.module'
+import { ArticleModule } from './article/article.module'
+import { CreateArticleModule } from './createArticle/createArticle.module'
+import { EditArticleModule } from 'src/app/editArticle/editArticle.module'
 
 @NgModule({
   declarations: [AppComponent],
@@ -22,7 +27,8 @@ import { StoreRouterConnectingModule } from '@ngrx/router-store';
     HttpClientModule,
     AppRoutingModule,
     AuthModule,
-    StoreModule.forRoot({}),
+    StoreModule.forRoot({ router: routerReducer }),
+    StoreRouterConnectingModule.forRoot(),
     StoreDevtoolsModule.instrument({
       maxAge: 25,
       logOnly: environment.production,
@@ -30,8 +36,11 @@ import { StoreRouterConnectingModule } from '@ngrx/router-store';
     EffectsModule.forRoot([]),
     TopBarModule,
     GlobalFeedModule,
-    TopBarModule,
-    StoreRouterConnectingModule.forRoot(),
+    YourFeedModule,
+    TagFeedModule,
+    CreateArticleModule,
+    ArticleModule,
+    EditArticleModule,
   ],
   providers: [
     PersistanceService,
